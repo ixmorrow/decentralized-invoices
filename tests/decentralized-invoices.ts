feb9e62810eab18920e8f5caaafee0ea9c6e6f18
ixmorrow <ivanmorrow14@gmail.com>
2022-11-21 13:01:32 -0600

    test create invoice

--- tests/decentralized-invoices.ts
+++ tests/decentralized-invoices.ts
@@ -1,16 +1,81 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { DecentralizedInvoices } from "../target/types/decentralized_invoices";
+import * as anchor from "@project-serum/anchor"
+import { Program } from "@project-serum/anchor"
+import { DecentralizedInvoices } from "../target/types/decentralized_invoices"
+import { PublicKey, Keypair, SystemProgram } from '@solana/web3.js'
+import { numbersToBuffer } from 'numbers-to-buffer'
+import { createMint, mintTo, getOrCreateAssociatedTokenAccount } from '@solana/spl-token'
+import { safeAirdrop, delay } from './utils/utils'
+import { token } from "@project-serum/anchor/dist/cjs/utils"
+import { merchantKeypair as merchant, customerKeypair as customer, mintAuth } from './test-keypairs/testKeypairs'
 
-describe("decentralized-invoices", () => {
-  // Configure the client to use the local cluster.
+describe("decentralized-invoices", async () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.DecentralizedInvoices as Program<DecentralizedInvoices>;
+  const provider = anchor.AnchorProvider.env()
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  let tokenMint = null
+  let merchantAta = null
+  let customerAta = null
+
+  it("Initialize accounts", async () => {
+    await safeAirdrop(merchant.publicKey, provider.connection)
+    await safeAirdrop(customer.publicKey, provider.connection)
+    await safeAirdrop(mintAuth.publicKey, provider.connection)
+    await safeAirdrop(provider.wallet.publicKey, provider.connection)
+    delay(10000)
+
+    tokenMint = await createMint(
+      provider.connection,
+      mintAuth,
+      mintAuth.publicKey,
+      mintAuth.publicKey,
+      9
+    )
+    console.log("Mint: ", tokenMint.toBase58())
+
+    merchantAta = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      merchant,
+      tokenMint,
+      merchant.publicKey
+      )
+    console.log("Merchant ata: ", merchantAta.address.toBase58())
+    customerAta = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      customer,
+      tokenMint,
+      customer.publicKey
+    )
+    console.log("Customer ata: ", customerAta.address.toBase58())
+
+    await mintTo(
+      provider.connection,
+      customer,
+      tokenMint,
+      customerAta.address,
+      mintAuth,
+      100
+    )
+  })
+
+  it("Create Invoice", async () => {
+    let uuid = new anchor.BN(1)
+    const [invoicePda, invoiceBump] = await PublicKey.findProgramAddress(
+      [uuid.toArrayLike(Buffer, 'le', 8), Buffer.from("invoice"), merchant.publicKey.toBuffer()],
+      program.programId
+    )
+    const tx = await program.methods.createInvoice(uuid, new anchor.BN(5))
+    .accounts({
+      merchant: merchant.publicKey,
+      customer: customer.publicKey,
+      invoice: invoicePda,
+      paymentMint: tokenMint,
+      systemProgram: SystemProgram.programId
+    })
+    .signers([merchant])
+    .rpc()
+    console.log("Your transaction signature", tx)
+  })
+
+})
